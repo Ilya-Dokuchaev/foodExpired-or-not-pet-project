b4661c3c542f21541d527677a6fbed91cb90db4d
Ilya Dokuchaev <miamorokor@mail.ru>
2022-05-01 21:23:08 +0300

    написана функция создания продукта и добавления его в LS

---
+++ index.js
@@ -0,0 +1,24 @@
+let foodStuffPieces = []; //массив куда все сохр
+
+const addFoodPiece = (ev) => {
+  ev.preventDefault(); // сброс перезагрузки по клику Записать
+  //создаем объект
+  let foodStuffPiece = {
+    namePiece: document.getElementById("namePiece").value,
+    bestbefore: document.getElementById("BB").value,
+  };
+  //пушим в массив
+  foodStuffPieces.push(foodStuffPiece);
+  // Сбрасываем форму
+  document.querySelector("form").reset();
+  // вывод массива доделать в читаемом виде
+  console.warn("added", { foodStuffPieces });
+  let pre = document.querySelector("#msg pre");
+  pre.textContent = "\n" + JSON.stringify(foodStuffPieces, "\t", 2);
+  localStorage.setItem("FoodStuffList ", JSON.stringify(foodStuffPieces));
+};
+// как то надо в память записывать и потом в ней работать
+console.log(foodStuffPieces);
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("btn").addEventListener("click", addFoodPiece);
+});
